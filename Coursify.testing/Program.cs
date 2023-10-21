9b4396a775249474af42e1a28e8b348b44f5ea8e
Bendawi <90578249+Bendawi@users.noreply.github.com>
2023-10-21 16:54:03 -0400

    Recusive method
    ja

--- Coursify.testing/Program.cs
+++ Coursify.testing/Program.cs
@@ -1,7 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Courseify.PdfMan;
+using static Courseify.PdfMan.IPdfBookmarkService;
 // Input PDF 
-Console.WriteLine("Hello, World!");
 PdfBookmarkService bookmarkService = new PdfBookmarkService();
 String PdfFilePath = "C:\\Users\\benda\\Downloads\\PHYSICS 150 BOOK.pdf";
-bookmarkService.GetBookmarksFromPdf(PdfFilePath); 
+var bookmarkReturn = bookmarkService.GetBookmarksFromPdf(PdfFilePath);
+tabsforchildren(bookmarkReturn);
+// Go trhoigh the list of nodes using a for loop 
+void tabsforchildren(BookmarkNode Node, int indentation =0)
+{
+    Console.WriteLine(new string('\t', indentation) + Node.Title);
+    foreach (var item in Node.Children)
+    {
+        tabsforchildren(item, indentation+1);
+    }
+}
