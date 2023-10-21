e5d53e84aa82835dc50bddc84ce844b59d16f937
Ali Bdeir <developer.ali.bdeir@gmail.com>
2023-10-21 15:43:24 -0400

    Updated services

--- Courseify.PdfMan/IPdfBookmarkService.cs
+++ Courseify.PdfMan/IPdfBookmarkService.cs
@@ -2,6 +2,11 @@
 {
     public interface IPdfBookmarkService
     {
+        /// <summary>
+        /// Gets the bookmarks from a pdf file
+        /// </summary>
+        /// <param name="inputFilePath">The path to the PDF file</param>
+        /// <returns>Dictionary with the name of each bookmark and the page it starts at</returns>
         Dictionary<string, int> GetBookmarksFromPdf(string inputFilePath);
     }
-}
+}   
