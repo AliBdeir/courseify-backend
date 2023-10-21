d996e55e4cfebbf378f65e13ba019d857a1a3863
Ali Bdeir <developer.ali.bdeir@gmail.com>
2023-10-21 14:19:59 -0400

    Version Controller

---
+++ CourseifyBackend/Controllers/VersionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseifyBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(System.Reflection.Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "No version found");
+        }
+    }
+}
